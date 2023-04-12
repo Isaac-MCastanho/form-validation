3f24222454080795a8afb00d72eb0889d615f233
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-04-11 23:42:48 -0300

    mudança no estilo do form
    Validação e transformação dos dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
+import { useState } from "react";
 import "./styles/global.css";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty("O nome é obrigatório")
+    .transform((name) => {
+      return name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1));
+        })
+        .join(" ");
+    }),
+  email: z
+    .string()
+    .nonempty("O e-mail é obrigatorio")
+    .email("Formato de e-mail inválido"),
+  password: z.string().min(6, "A senha precisa de no mínimo 6 caracteres"),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
+
 function App() {
+  const [output, setOutput] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
   return (
-    <main className="h-screen bg-zinc-950 text-zinc-300 flex items-center justify-center">
-      <form action="" className="flex flex-col gap-4 w-full max-w-xs">
+    <main className="h-screen bg-zinc-950 text-zinc-300 flex flex-col gap-10 items-center justify-center">
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col gap-4 w-full max-w-xs"
+      >
         <div className="flex flex-col gap-1">
-          <label htmlFor="">E-mail</label>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            className="border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+            {...register("name")}
+          />
+
+          {errors.name && (
+            <span className="text-red-700">{errors.name.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">E-mail</label>
           <input
             type="email"
-            className="border border-zinc-20 shadow-sm rounded h-10 px-3"
-            name="email"
+            className="border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+            {...register("email")}
           />
+
+          {errors.email && (
+            <span className="text-red-700">{errors.email.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Senha</label>
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
-            className="border border-zinc-20 shadow-sm rounded h-10 px-3"
-            name="password"
+            className="border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+            {...register("password")}
           />
+          {errors.password && (
+            <span className="text-red-700">{errors.password.message}</span>
+          )}
         </div>
 
         <button
@@ -29,6 +91,8 @@ function App() {
           Enviar
         </button>
       </form>
+
+      <pre>{output}</pre>
     </main>
   );
 }
